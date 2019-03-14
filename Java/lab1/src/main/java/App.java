87f00b7979b2b4069146a48289018711bb7e4767
VaeraTM <v.malkhanov@g.nsu.ru>
2019-03-14 11:42:44 +0700

    Version2.0

--- Java/lab1/src/main/java/App.java
+++ Java/lab1/src/main/java/App.java
@@ -9,21 +9,19 @@ public class App {
             System.out.println("First argument: Input File");
             System.out.println("Second argument: Buffer Size");
         }
-		else {
-			try {
-				int size = Integer.parseUnsignedInt(args[1]);
-				if (size == 0) {
-				    throw new NumberFormatException("Zero");
-				}
-				Controller boss = new Controller(new FileInputStream(args[0]), new FileOutputStream("out.csv"), size);
-			}
-			catch (NumberFormatException ex) {
-				System.err.println(ex.getMessage());
-				System.err.println("Second argument must be unsigned integer number > 0");
-			}
-			catch (IOException ex) {
-				System.err.println(ex.getMessage());
-			}
-		}
+        else {
+            try {
+                int size = Integer.parseUnsignedInt(args[1]);
+                if (size == 0) {
+                    throw new NumberFormatException("Zero");
+                }
+                Controller boss = new Controller(new FileInputStream(args[0]), new FileOutputStream("out.csv"), size);
+            } catch (NumberFormatException ex) {
+                System.err.println(ex.getMessage());
+                System.err.println("Second argument must be unsigned integer number > 0");
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
     }
 }
